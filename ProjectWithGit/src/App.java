1dffa1074526efaec9bb366955862adc20797ac6
DTorres97 <danilo97@my.yorku.ca>
2017-01-15 17:45:27 -0500

    Added some buttons to our GUI.

---
+++ ProjectWithGit/src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class App{
+	
+	public static void main(String args[]){
+	//creates frame
+		ProjectWithGit project=new ProjectWithGit();
+		project.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		project.setSize(1000,1000);
+		project.setVisible(true);
+
+	}
+	
+}
--- ProjectWithGit/src/ProjectWithGit.java
+++ ProjectWithGit/src/ProjectWithGit.java
@@ -1,8 +1,51 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class ProjectWithGit {
+public class ProjectWithGit extends JFrame{
+ 
+	//initialize buttons
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton button4;	
+	private JButton button5;
+	
+	//constructor
+	public ProjectWithGit(){
+	//title of frame
+	super("Starting Out With Some Buttons");
+	setLayout(new FlowLayout());
+	
+	//adds buttons to frame
+	button1=new JButton("1");
+	button1.setBackground(Color.WHITE);
+	button1.setForeground(Color.BLACK);
+	button1.setFont(new Font("Arial", Font.PLAIN, 100));
+	add(button1);
 
-	public static void main(String[] args) {
-		System.out.println("Fun with Gitq2345678");
-	}
+	button2=new JButton("2");
+	button2.setBackground(Color.WHITE);
+	button2.setForeground(Color.BLACK);
+	button2.setFont(new Font("Arial", Font.PLAIN, 100));
+	add(button2);
 
-}
+	button3=new JButton("3");
+	button3.setBackground(Color.WHITE);
+	button3.setForeground(Color.BLACK);
+	button3.setFont(new Font("Arial", Font.PLAIN, 100));
+	add(button3);
+
+	button4= new JButton("4");
+	button4.setBackground(Color.WHITE);
+	button4.setForeground(Color.BLACK);
+	button4.setFont(new Font("Arial", Font.PLAIN, 100));
+	add(button4);
+
+	button5= new JButton("5");
+	button5.setBackground(Color.WHITE);
+	button5.setForeground(Color.BLACK);
+	button5.setFont(new Font("Arial", Font.PLAIN, 100));
+	add(button5);
+            }
+}	
