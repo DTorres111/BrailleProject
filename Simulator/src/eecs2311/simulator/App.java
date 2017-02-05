be93b683b27d6540f171174753678724a1323ea1
RABIA <RABIA@192.168.0.24>
2017-02-04 23:04:58 -0500

    Final

--- Simulator/src/eecs2311/simulator/App.java
+++ Simulator/src/eecs2311/simulator/App.java
@@ -11,10 +11,9 @@ public class App {
 					SimulatorAPI window = new SimulatorAPI(6,8,5);
 					window.getFrame().setVisible(true);
 					
-					//window.setLetter('a', 1);
-					//window.setLetter('o', 2);
-					//window.setLetter('s', 3);
-					window.setWord("list");
+					window.setLetter('a', 1);
+					window.setLetter('o', 2);
+					window.setLetter('s', 3);
 					window.setPin(8, 8);
 					window.enableButton(6);
 			
