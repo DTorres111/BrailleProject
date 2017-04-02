0ca05fcd63aecd8004db21ba4a5b3fe99fad579b
usman <usman@192.168.2.18>
2017-04-02 06:29:29 -0400

    COnversion to scenario file working going to add remaining stuff soon
    Try typing in a scenario from the given ones in github

--- BrailleProject/src/Authoring/Frame.java
+++ BrailleProject/src/Authoring/Frame.java
@@ -49,9 +49,8 @@ public class Frame {
 	int c;
 	int q;
 	int t;
-	
-	
-	 
+	JButton sound;
+
 	// for the scenario file
 	JFrame frames = new JFrame();
 	JTextArea sce = new JTextArea(10, 10);
@@ -70,12 +69,16 @@ public class Frame {
 		JButton button = new JButton("Message");
 		JButton delete = new JButton("Delete");
 		JButton question = new JButton("Question");
-		JButton sound = new JButton("Sound");
+		sound = new JButton("Sound");
 		JButton voice = new JButton("Voice");
 		JButton reorder = new JButton("Reorder");
 		JButton edit = new JButton("Edit");
 		JButton repeat = new JButton("Repeat");
-		
+		JButton charsetting = new JButton("Char BCELL");
+		JButton stringsetting = new JButton("String BCELL");
+		JButton choices = new JButton("Set choices");
+		JButton choice = new JButton("Choice Scenario");
+
 		GridBagConstraints gg = new GridBagConstraints();
 		// gg.fill = GridBagConstraints.HORIZONTAL;
 		gg.weightx = 0;
@@ -114,12 +117,28 @@ public class Frame {
 		gg.gridx = 7;
 		gg.gridy = 0;
 		panel.add(edit, gg);
-        
+
 		gg.gridx = 8;
 		gg.gridy = 0;
 		panel.add(repeat, gg);
 
-   		// panel for log
+		gg.gridx = 9;
+		gg.gridy = 0;
+		panel.add(charsetting, gg);
+
+		gg.gridx = 10;
+		gg.gridy = 0;
+		panel.add(stringsetting, gg);
+
+		gg.gridx = 11;
+		gg.gridy = 0;
+		panel.add(choices, gg);
+
+		gg.gridx = 12;
+		gg.gridy = 0;
+		panel.add(choice, gg);
+
+		// panel for log
 
 		log.setLineWrap(true);
 		log.setEditable(false);
@@ -159,8 +178,7 @@ public class Frame {
 		result.add("Cell");
 		result.add("Button");
 		sce.setText(String.join("\n", result));
-		
-		
+
 		// buttons action listener
 
 		button.addActionListener(new ActionListener() {
@@ -268,25 +286,48 @@ public class Frame {
 				no();
 			}
 		});
-		
+
 		repeat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				repeat();
 			}
 		});
-		
+
 		sound.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				soundbutton();
 			}
 		});
-		
+
 		voice.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				voicebutton();
 			}
 		});
-		
+
+		charsetting.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				voicebutton();
+			}
+		});
+
+		stringsetting.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stringsetting();
+			}
+		});
+
+		choices.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choices();
+			}
+		});
+
+		choice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choice();
+			}
+		});
 
 	}
 
@@ -436,7 +477,7 @@ public class Frame {
 			String a = "Question: " + d.getText();
 			message.set(c, a);
 		}
-        
+
 		frame3.dispose();
 		frame.setVisible(true);
 
@@ -501,8 +542,8 @@ public class Frame {
 		}
 
 	}
-	
-	public int scenario3(){
+
+	public int scenario3() {
 		int i = 1;
 
 		for (int start = 0; start < c; start++) {
@@ -513,31 +554,30 @@ public class Frame {
 		}
 		return i;
 	}
-	
-	public void scenario4(){
-		int number=scenario3();
-		int e=0;
-		String w=message.get(c);
-		String[] y=w.split(" ",2);
-		if(number-1==0){
-			int b=result.indexOf(y[1]);
-			result.set(b,d.getText());
-		}
-		else{
+
+	public void scenario4() {
+		int number = scenario3();
+		int e = 0;
+		String w = message.get(c);
+		String[] y = w.split(" ", 2);
+		if (number - 1 == 0) {
+			int b = result.indexOf(y[1]);
+			result.set(b, d.getText());
+		} else {
 			for (int i = 0; number != e; i++) {
 
 				if (result.get(i).equals(y[1])) {
 					e++;
 					if (e == number) {
-						result.set(i,d.getText());
+						result.set(i, d.getText());
 					}
 				}
 
 			}
 		}
 	}
-	
-	public int scenario5(){
+
+	public int scenario5() {
 		int i = 1;
 
 		for (int start = 0; start < q; start++) {
@@ -548,8 +588,8 @@ public class Frame {
 		}
 		return i;
 	}
-	
-	public int scenario6(){
+
+	public int scenario6() {
 		int i = 1;
 
 		for (int start = 0; start < t; start++) {
@@ -560,121 +600,343 @@ public class Frame {
 		}
 		return i;
 	}
-	
-	public void scenario7(){
-		int a=scenario5();
-		int b=scenario6();
-		String c=message.get(q);
-		String d=message.get(t);
-		String[] e=c.split(" ",2);
-		String[] f=d.split(" ",2);
-		int x=0;
-		int alpha=0;
-		int beta=0;
-		int aaa=0;
-		if(a-1==0 && b-1==0){
-			int g=result.indexOf(e[1]);
-			int h=result.indexOf(f[1]);
+
+	public void scenario7() {
+		int a = scenario5();
+		int b = scenario6();
+		String c = message.get(q);
+		String d = message.get(t);
+		String[] e = c.split(" ", 2);
+		String[] f = d.split(" ", 2);
+		int x = 0;
+		int alpha = 0;
+		int beta = 0;
+		int aaa = 0;
+		if (a - 1 == 0 && b - 1 == 0) {
+			int g = result.indexOf(e[1]);
+			int h = result.indexOf(f[1]);
 			result.set(g, f[1]);
 			result.set(h, e[1]);
 		}
-		
-		else if(a-1==0 && b-1!=0){
+
+		else if (a - 1 == 0 && b - 1 != 0) {
 			for (int i = 0; b != x; i++) {
 
 				if (result.get(i).equals(f[1])) {
 					x++;
 					if (x == b) {
-					    int hh=result.indexOf(e[1]);
-						result.set(hh,f[1]);
-						result.set(i,e[1]);
+						int hh = result.indexOf(e[1]);
+						result.set(hh, f[1]);
+						result.set(i, e[1]);
 					}
 				}
+			}
 		}
-	}
-		
-		else if(a-1!=0 && b-1==0){
+
+		else if (a - 1 != 0 && b - 1 == 0) {
 			for (int i = 0; a != x; i++) {
 
 				if (result.get(i).equals(e[1])) {
 					x++;
 					if (x == a) {
-					    int hh=result.indexOf(f[1]);
-						result.set(hh,e[1]);
-						result.set(i,f[1]);
+						int hh = result.indexOf(f[1]);
+						result.set(hh, e[1]);
+						result.set(i, f[1]);
 					}
 				}
-		}
-	}
-		else{
+			}
+		} else {
 			for (int i = 0; a != x; i++) {
 
 				if (result.get(i).equals(e[1])) {
 					x++;
 					if (x == a) {
-					    alpha=i;
+						alpha = i;
 					}
 				}
 
-		}
-	
+			}
+
 			for (int i = 0; b != aaa; i++) {
-                   
+
 				if (result.get(i).equals(f[1])) {
 					aaa++;
 					if (x == b) {
-					   beta=i;
+						beta = i;
 					}
 				}
 
-}
-			result.set(alpha,f[1]);
-			result.set(beta,e[1]);
-			System.out.println(alpha+"/n"+beta);
+			}
+			result.set(alpha, f[1]);
+			result.set(beta, e[1]);
+			System.out.println(alpha + "/n" + beta);
+		}
 	}
-}
-	
-	public void repeat(){
-		JFrame frame=new JFrame();
+
+	public void repeat() {
+		JFrame frame = new JFrame();
 		frame.setVisible(true);
-		JPanel panel=new JPanel();
-		JButton srepeat= new JButton("Start Repeat");
-		JButton strepeat= new JButton("Stop Repeat");
+		JPanel panel = new JPanel();
+		JButton srepeat = new JButton("Start Repeat");
+		JButton strepeat = new JButton("Stop Repeat");
+		
+		
 		panel.add(srepeat);
 		panel.add(strepeat);
 		frame.add(panel);
-		
+
 		srepeat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				start();
 			}
 		});
-		
+
 		strepeat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				stop();
 			}
 		});
-		
+
 	}
-	
-	public void start(){
+
+	public void start() {
 		message.add("Repeat Started");
 		result.add("/~repeat");
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		message.add("Repeat Stopped");
 		result.add("/~endrepeat");
+		result.add("");
+		JFrame frame=new JFrame();
+		frame.setVisible(true);
+		JPanel panel=new JPanel();
+		JLabel label=new JLabel("Button for repeat(Not button 1 or 2):");
+	    JTextField text=new JTextField(10);
+	    JButton ok=new JButton("ok");
+	    panel.add(label);
+	    panel.add(text);
+	    panel.add(ok);
+	    frame.add(panel);
+	    ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				result.add("");
+				int a=Integer.parseInt(text.getText())-1;
+				String b=Integer.toString(a);
+				result.add("/~repeat-button:"+b);
+			}
+		});
+		
 	}
-	
-	public void soundbutton(){
+
+	public void soundbutton() {
 		message.add("Sound added");
 		result.add("/~sound:");
 	}
-	
-	public void voicebutton(){
+
+	public void voicebutton() {
 		message.add("Voice added");
 		result.add("/~set-voice:");
 	}
+
+	public void stringsetting() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		JTextField d = new JTextField(10);
+		JLabel e = new JLabel("Type text here to be set on Braille cell:");
+		JButton ok = new JButton("OK");
+		panel.add(ok);
+		panel.add(d);
+		panel.add(e);
+		frame.add(panel);
+
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				message.add(d.getText());
+				result.add("/~disp-string:" + d.getText());
+			}
+		});
+	}
+
+	public void choices() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel(new GridBagLayout());
+		JTextField d = new JTextField(10);
+		JLabel e = new JLabel("Button for correct choice:");
+		JTextField f = new JTextField(10);
+		JLabel g = new JLabel("Button for wrong choice:");
+		JButton ok = new JButton("OK");
+		GridBagConstraints gg = new GridBagConstraints();
+		gg.weightx = 0;
+		gg.gridx = 1;
+		gg.gridy = 1;
+		panel.add(d, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 1;
+		panel.add(e, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 0;
+		panel.add(f, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 0;
+		panel.add(g, gg);
+
+		gg.gridx = 2;
+		gg.gridy = 1;
+		panel.add(ok, gg);
+
+		frame.add(panel);
+
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				message.add("Button " + d.getText() + " set as correct option.");
+				message.add("Button " + f.getText() + " set as incorrect option.");
+				int a = Integer.parseInt(d.getText()) - 1;
+				int b = Integer.parseInt(f.getText()) - 1;
+				String c = Integer.toString(a);
+				String d = Integer.toString(b);
+				result.add("/~skip-button:" + c + " ONEE");
+				result.add("/~skip-button:" + d + " TWOO");
+				result.add("/~user-input");
+                result.add("");
+			}
+		});
+
+	}
+
+	public void choice() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel(new GridBagLayout());
+		JLabel e = new JLabel(
+				"Type in al the details for the correct/incorrect scenario. Be sure to make no mistake as this part cannot be edited later.");
+		JLabel a = new JLabel("Scenario for correct option");
+		JLabel g = new JLabel("Button for correct option(Just type in button number):");
+		JTextField h = new JTextField(10);
+		JLabel c = new JLabel("Add sound by pressing sound button");
+		JLabel d = new JLabel("Type any message here:");
+		JTextField f = new JTextField(10);
+		JLabel b = new JLabel("Scenario for incorrect option");
+		JLabel i = new JLabel("Button for incorrect option(Just type in button number):");
+		JTextField j = new JTextField(10);
+		JLabel k = new JLabel("Add sound by pressing sound button");
+		JLabel l = new JLabel("Type any message here:");
+		JTextField m = new JTextField(10);
+		JButton ok = new JButton("Ok");
+
+		GridBagConstraints gg = new GridBagConstraints();
+	
+		gg.weightx = 0;
+		
+		gg.gridx = 0;
+		gg.gridy = 10;
+		panel.add(e, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 9;
+		panel.add(a, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 8;
+		panel.add(g, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 8;
+		panel.add(h, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 7;
+		panel.add(c, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 7;
+		panel.add(sound, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 6;
+		panel.add(d, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 6;
+		panel.add(f, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 5;
+		panel.add(b, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 4;
+		panel.add(i, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 4;
+		panel.add(j, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 3;
+		panel.add(k, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 3;
+		panel.add(sound, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 2;
+		panel.add(l, gg);
+
+		gg.gridx = 1;
+		gg.gridy = 2;
+		panel.add(m, gg);
+
+		gg.gridx = 0;
+		gg.gridy = 1;
+		panel.add(ok, gg);
+
+		frame.add(panel);
+
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				message.add("Correct scenario added.\n  Sound added \n  Message: " + f.getText());
+				if (h.getText().equals("1")) {
+					result.add("/~ONEE");
+					result.add("/~sound:");
+					result.add(f.getText());
+					result.add("/~skip:NEXTT");
+					result.add("");
+				} else if (h.getText().equals("2")) {
+					result.add("/~TWOO");
+					result.add("/~sound:");
+					result.add(f.getText());
+					result.add("/~skip:NEXTT");
+					result.add("");
+				}
+
+				message.add("Incorrect scenario added.\n  Sound added \n  Message: " + m.getText());
+					if (j.getText().equals("2")) {
+						result.add("/~TWOO");
+						result.add("/~sound:");
+						result.add(m.getText());
+						result.add("/~skip:NEXTT");
+					} else if (j.getText().equals("1")) {
+						result.add("/~ONEE");
+						result.add("/~sound:");
+						result.add(m.getText());
+						result.add("/~skip:NEXTT");
+					}
+                
+				 result.add("");	
+                 result.add("/~NEXTT");
+                 result.add("");
+                 result.add("/~disp-clearALL");
+                 result.add("/~reset-buttons");
+			}
+		});
+
+	}
+
 }
